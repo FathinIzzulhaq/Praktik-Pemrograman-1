87f5a82f0723018a0808a3059d9cb1145159589a
Your Name <you@example.com>
2021-09-14 13:59:49 +0700

    Mencoba menambhkan Informasi pribadi saya

---
+++ Prak pemrog 3/cobaVar.java
@@ -0,0 +1,12 @@
+public class cobaVar {
+    public static void main(String[] args) {
+        String namaPanggilan = "Fathin";
+        int umur = 18;
+        double tinggiBadan = 173;
+        System.out.println("Perkenalkan nama saya " + namaPanggilan);
+        System.out.println("Umur saya adalah " + umur);
+        System.out.println("Saya memiliki Tinggi badan " + tinggiBadan);
+
+    }
+    
+}
