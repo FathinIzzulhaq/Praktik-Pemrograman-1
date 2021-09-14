0a53b2481f0b96ba5bf27866c7e4ff3a3abad82d
Your Name <you@example.com>
2021-09-14 13:15:59 +0700

    Mencoba menambahkan NIM dan Nama

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+ /*
+ NIM : 21537144017
+ Nama: Fathin
+ */
 public class Hello {
     public static void main(String[] args) {
+        // untuk menampilkan teks
         System.out.println("Hello world");
     }
 }
