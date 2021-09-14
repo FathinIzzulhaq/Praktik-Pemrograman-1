b774a1ebf06c973b16484aadeb88a7dc4fd83f2e
Your Name <you@example.com>
2021-09-14 14:06:28 +0700

    Mencoba nama file dengan isi informasi pribadi

---
+++ Prak pemrog 3/mencobavariabel.py
@@ -0,0 +1,11 @@
+namaPanggilan = "Fathin"
+umur = 18
+tinggiBadan = 173
+print(namaPanggilan)
+print(umur)
+print(tinggiBadan)
+print(type{namaPanggilan})
+print(type{umur})
+print(type{tinggiBadan})
+
+
