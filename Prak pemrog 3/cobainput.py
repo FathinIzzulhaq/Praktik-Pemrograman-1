9a909e4a49d5148600d33ff2744b453e6e6224dd
Your Name <you@example.com>
2021-09-26 10:05:31 +0700

    coba

---
+++ Prak pemrog 3/cobainput.py
@@ -0,0 +1,13 @@
+print("Masukkan nama anda : ")
+namaDepan = input()
+print("Masukkan tinggi anda : ")
+tinggi = input()
+print("Masukkan alamat anda : ")
+alamat = input()
+
+print("Nama saya adalah" + namaDepan)
+print("Tinggi anda adalah" + tinggi)
+print("Alamat anda adalah" + alamat)
+
+
+
---
+++ Prak pemrog 3/pertemuan4/Cobainput.java
@@ -0,0 +1,16 @@
+public class Cobainput {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Sysytem.out.print("Masukkan nama anda");
+        String namaDepan = scanner.nextLine();
+
+        System.out.print("Masukkan umur anda");
+        int umur = scanner.nextInt();
+
+        System.out.println("Nama anda adalah" + namaDepan);
+        System.out.println("Umur anda adalah"+ umur);
+        
+    } 
+}
