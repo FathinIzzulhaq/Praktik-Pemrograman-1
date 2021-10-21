218af486937ff59861837f25f0cfe3d5965f6b3f
Your Name <you@example.com>
2021-10-21 20:43:51 +0700

    Belajar Array Input java

--- Pertemuan 8/BelajarArrayInput.java
+++ Pertemuan 8/BelajarArrayInput.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 public class BelajarArrayInput {
     public static void main(String[] args) {
         int[] bilangan = new int[5];
-        for(int i=0; i<5; i++) {
-            Scanner scan = new Scanner9System.in);
+        for (int i=0; i<5; i++) {
+            Scanner scan = new Scanner(System.in);
             System.out.print("masukan bilangan ke-"+i+"=");
-            bilangan [i]=scan.nextInt();
+            bilangan[i]=scan.nextInt();
 
         }
     for (int i=0; i<bilangan.length; i++) {
-System.out.println("Bilangan ke-"+i+"=" +bilangan[i]);
+System.out.println("Bilangan ke-"+i+" = " +bilangan[i]);
 
     }
+
     }
 
 }
